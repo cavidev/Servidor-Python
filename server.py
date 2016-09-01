bd677a31bdb9db7cf476122e470143b0ac7c30d3
Carlos Villafuerte <carlosmario.villafuerted66@gmail.com>
2016-09-01 11:07:37 -0600

    Actualizacion del servidor y textos de referencias

--- server.py
+++ server.py
@@ -1,23 +1,36 @@
 __author__ = 'Carlos Mario'
-from http.server import  BaseHTTPRequestHandler, HTTPServer
+import web
 
-class http_server:
-    def __init__(self):
-        server = HTTPServer(('', 5050), myHandler)
-        server.serve_forever()
+urls = (
+  '/', 'index'    )
 
-class myHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        # Hacer lo que queramos con la petición
-        return
+class index:
+    def GET(self):
+        print ("Hello, world!")
 
-class main:
-    def __init__(self):
-        self.server = http_server()
+if __name__ == "__main__": web.run(urls, globals())
 
 
-if __name__ == '__main__':
-    m = main()
+
+# from http.server import  BaseHTTPRequestHandler, HTTPServer
+#
+# class http_server:
+#     def __init__(self):
+#         server = HTTPServer(('', 5050), myHandler)
+#         server.serve_forever()
+#
+# class myHandler(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/html')
+#         self.end_headers()
+#         # Hacer lo que queramos con la petición
+#         return
+#
+# class main:
+#     def __init__(self):
+#         self.server = http_server()
+#
+#
+# if __name__ == '__main__':
+#     m = main()
