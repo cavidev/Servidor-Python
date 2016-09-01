666cbfc751b52fb5853fc4cc9a356e52fb087d7d
Carlos Villafuerte <carlosmario.villafuerted66@gmail.com>
2016-09-01 00:38:45 -0600

    Archivo para el servidor

---
+++ server.py
@@ -0,0 +1,23 @@
+__author__ = 'Carlos Mario'
+from http.server import  BaseHTTPRequestHandler, HTTPServer
+
+class http_server:
+    def __init__(self):
+        server = HTTPServer(('', 5050), myHandler)
+        server.serve_forever()
+
+class myHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Hacer lo que queramos con la petición
+        return
+
+class main:
+    def __init__(self):
+        self.server = http_server()
+
+
+if __name__ == '__main__':
+    m = main()
