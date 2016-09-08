10b3a13fe24790a8ca2d70cd75ef258a1a0bc38c
Carlos Villafuerte <carlosmario.villafuerted66@gmail.com>
2016-09-08 01:47:58 -0600

    Web service funcional, solo ejecutelo normalmente. no pude hacer lo de las imagenes, tampoco lo de angular se va a tener que trabajar todo por flask

--- server.py
+++ server.py
@@ -1,12 +1,8 @@
-import base64
 import io
 import os
 import base64
-from flask import Flask,request,render_template,jsonify,\
-    redirect, url_for # Esta siempre va en proyectos de flask
+from flask import Flask,request,render_template,jsonify,redirect, url_for # Esta siempre va en proyectos de flask
 from flask import flash
-from flask import send_from_directory
-from wand.image import Image
 from werkzeug.utils import secure_filename
 from manager import *
 
@@ -40,14 +36,13 @@ def allowed_file(filename):
 @app.route('/register', methods=['GET','POST'])
 def register():
     if 'fotoSubida' not in request.files:
-        flash('No file part')
         return redirect(request.url)
     nombreCompleto = request.form['nombreCompleto']
     usuario = request.form['usuario']
     contrasena = request.form['contrasena']
     permiso = request.form['permiso']
     foto = request.files['fotoSubida'].read()
-    foto1=request.files['fotoSubida']
+    foto1 = request.files['fotoSubida']
 
     if foto1.filename == '':
          flash('No selected file')
@@ -57,8 +52,12 @@ def register():
          foto1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
          #InsertarUsuarioManager(usuario, contrasena, nombreCompleto, permiso, foto)
     bytes = bytearray(foto)
-    image = Image.open(io.BytesIO(bytes))
-    return render_template("profile.html", suceso=image)
+    #imagen64 = base64.encodebytes(foto)
+    imagen64 = base64.b64encode(foto)
+    print(imagen64)
+
+    imagen = base64.decodebytes(imagen64)
+    return render_template("profile.html", suceso=imagen64)
 
 
 
@@ -89,41 +88,6 @@ def register():
 # @app.route('/uploads/<filename>')
 # def send_file(filename):
 #     return send_from_directory(UPLOAD_FOLDER, filename)
-
-
-
-@app.route("/nombre")
-@app.route("/nombre/<name>")  # Toma la url + una variable y la devuelve al html designado.
-def nombre(name=None):
-    return render_template("profile.html", name=name)
-
-<<<<<<< HEAD
-
-@app.route("/procesar", methods=['GET','POST'])
-def procesar():
-    return render_template("profile.html", name="Hola")
-
-@app.route("/compras", methods=['GET','POST'])  # Pasando objetos. Una lista de productos.
-def compras():
-    apellidoE = request.form['name']
-    print(apellidoE)
-    return render_template("comprando.html", comida=salude(), persona=persona1, nombreE=apellidoE)
-
-
-@app.route('/_add_numbers')
-def add_numbers():
-    """Add two numbers server side, ridiculous but well..."""
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
-
-
-# Siempre va la ruta y justo despues la funcion que se dispara cuando en el navegador se pone esa ruta
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
 
 
 # Esto lo hace correr. El debug = true permite que se impriman los errores, si se quita igual funciona
